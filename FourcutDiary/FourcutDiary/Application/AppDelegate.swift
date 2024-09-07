5be48dd60968c761a9ef99e55c53f2eacc8de91b
yungu0010 <dbstj16@sookmyung.ac.kr>
2024-09-08 00:05:07 +0900

    [Feat] #3 - kakao_native_app_key xcconfig와 plist로 접근하도록 수정

--- FourcutDiary/FourcutDiary/Application/AppDelegate.swift
+++ FourcutDiary/FourcutDiary/Application/AppDelegate.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import KakaoSDKCommon
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+	
+		KakaoSDK.initSDK(appKey: Config.kakaoAppKey)
+		
 		return true
 	}
 
--- FourcutDiary/FourcutDiary/Application/SceneDelegate.swift
+++ FourcutDiary/FourcutDiary/Application/SceneDelegate.swift
@@ -6,17 +6,25 @@
 //
 
 import UIKit
+import KakaoSDKAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 	var window: UIWindow?
 
-
+	func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+		if let url = URLContexts.first?.url {
+			if (AuthApi.isKakaoTalkLoginUrl(url)) {
+				_ = AuthController.handleOpenUrl(url: url)
+			}
+		}
+	}
+	
 	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 		guard let windowScene = scene as? UIWindowScene else { return }
 		
 		let window = UIWindow(windowScene: windowScene)
-		window.rootViewController = AuthViewController()
+		window.rootViewController = AuthViewController(viewModel: AuthViewModelImpl(authManager: OAuthManagerImpl(appleLoginManager: AppleLoginManager(), kakaoLoginManager: KakaoLoginManager())))
 		window.makeKeyAndVisible()
 		self.window = window
 	}
